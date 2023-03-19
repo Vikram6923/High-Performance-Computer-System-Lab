5b7a9bd83646087e5b38e4950f5bb1f5545d624d
Vikram Aditya Singh <vikram6923@gmail.com>
2023-03-19 18:32:20 +0530

    Done prefixsum assignment

--- Lab8/prefixsum.cpp
+++ Lab8/prefixsum.cpp
@@ -16,17 +16,21 @@ int identity(int x) {
     return x;
 }
 
-int* nearestPowerOfTwo(int &n, int* A) {
+int red_func(int x) {
+    return x != -1 ? 1 : 0;
+}
+
+vector<int> nearestPowerOfTwo(int& n, vector<int> A) {
     int a = log2(n);
     if (pow(2, a) == n)
         return A;
     n = pow(2, a + 1);
-    int* newA = new int[n]();
-    std::copy(A, A + n, newA);
+    vector<int> newA(n, 0);
+    copy(A.begin(), A.end(), newA.begin());
     return newA;
 }
 
-int* inplace_prefix_sum(int A[], int n) {
+vector<int> inplace_prefix_sum(vector<int> A, int n, int (*f2)(int, int)) {
     int x = 2;
     // Resize the array to the nearest next power of 2
     A = nearestPowerOfTwo(n, A);
@@ -34,45 +38,27 @@ int* inplace_prefix_sum(int A[], int n) {
     while (x <= n) {
         #pragma pragma omp parallel for collapse(2)
         for (int i = x - 1; i < n; i += x) {
-            for (int j = i - x/2; j < i; j++)
-                A[j + 1] += A[i - x/2];
+            for (int j = i - x/2; j < i; j++) {
+                // A[j + 1] += A[i - x/2];
+                A[j + 1] = f2(A[j + 1], A[i - x/2]);
+            }
         }
         x = x * 2;
     }
     return A;
 }
 
-int* inplace_prefix_sum_p(int A[], int n) {
-    int x = 1;
-    // Resize the array to the nearest next power of 2
-    A = nearestPowerOfTwo(n, A);
-
-    int* B = new int[n];
-    std::copy(A, A + n, B);
-
-    while (x <= n) {
-        #pragma omp parallel for 
-        for (int i = x - 1; i < n; i++) {
-            B[i] += A[i - x];
-        }
-        std::copy(B, B + n, A);
-        x = x * 2;
-    }
-    return A;
-}
-
-
-int* prefix_sum(int A[], int n, int (*f1)(int), int (*f2)(int, int)) {
+vector<int> prefix_sum(vector<int> A, int n, int (*f1)(int), int (*f2)(int, int)) {
     // Resize the array to the nearest next power of 2
     int a = log2(n);
     if (pow(2, a) != n)
         A = nearestPowerOfTwo(n, A);
     int log2n = log2(n);
 
-    int* M = new int[2*n];
-    // int* L = new int[n];
-    int* R = new int[2* n];
-    int* prefixsum = new int[n];
+    vector<int> M(2*n);
+    //vector<int> L(n);
+    vector<int> R(2*n);
+    vector<int> prefixsum(n);
 
     // Bottom-up phase
     M[0] = 0;
@@ -85,16 +71,23 @@ int* prefix_sum(int A[], int n, int (*f1)(int), int (*f2)(int, int)) {
     //     cout << x << " ";
     // cout << endl;
 
-    int offsets[log2n + 1];
-    int offset = n;
-    for (int i = 0; i <= log2n; i++) {
+    int offsets[log2n + 2];
+    int offset = 2*n;
+    #pragma omp parallel for schedule(dynamic, 1024)
+    for (int i = 0; i <= log2n + 1; i++) {
         offsets[i] = offset;
         offset = offset >> 1;
     }
 
+    // cout << "offsets: ";
+    // for (int i = 0; i <= log2n; i++) {
+    //     cout << offsets[i] << " ";
+    // }
+    // cout << endl;
+
     for (int r = 1; r <= log2n; r++) {
         #pragma omp parallel for schedule(dynamic, 1024)
-        for (int i = offsets[r]; i < offsets[r-1]; i += 2) {
+        for (int i = offsets[r + 1]; i < offsets[r]; i += 2) {
             M[i] = f2(M[2 * i], M[2 * i + 1]);
             M[i + 1] = f2(M[2 * (i + 1)], M[2 * (i + 1) + 1]);
         }
@@ -111,15 +104,18 @@ int* prefix_sum(int A[], int n, int (*f1)(int), int (*f2)(int, int)) {
     // Final phase
 
     R[0] = R[1] = 0;
-    int m = 2;
+    // int m = log2n - 1;
+    int m = log2n + 1;
     for (int r = 2; r <= log2n + 1; r++) {
         #pragma omp parallel for schedule(dynamic, 1024)
-        for (int i = m; i < 2 * m; i+=2) {
-            R[i] = R[i/2];
-            R[i+1] = f2(R[i/2], M[i]);
+        for (int i = offsets[m]; i < offsets[m - 1]; i++) {
+            R[2*i] = R[i];
+            R[2*i + 1] = f2(R[i], M[2*i]);
         }
-        m *= 2;
+        // cout << offsets[m] << " ";
+        m--;
     }
+    // cout << endl;
     // cout << "R: ";
     // for (auto x: R)
     //     cout << x << " ";
@@ -131,41 +127,105 @@ int* prefix_sum(int A[], int n, int (*f1)(int), int (*f2)(int, int)) {
         prefixsum[i] = f2(M[i + n], R[i + n]);
     }
 
-    // Resize prefixsum to the original size of A
-    // prefixsum.resize(t);
-    delete[] M;
-    // delete[] L;
-    delete[] R;
     return prefixsum;
 }
 
 
-int* sequential_prefix_sum(int A[], int n, int (*f2)(int, int)) {
-    int* prefixsum = new int[n];
+vector<int> sequential_prefix_sum(vector<int> A, int n, int (*f2)(int, int)) {
+    // int* prefixsum = new int[n];
+    vector<int> prefixsum(n);
     prefixsum[0] = A[0];
     for (int i = 1; i < n; i++) {
-        prefixsum[i] = prefixsum[i - 1] + A[i];
+        prefixsum[i] = f2(prefixsum[i - 1], A[i]);
     }
     return prefixsum;
 }
 
-pair<int*, size_t> reduce_array(int A[], int n) {
-    int* B = new int[n]();
+void compact_array(int n) {
+    // Initialize array A with random values
+    vector<int> A(n);
+    for (int i = 0; i < n; i++) {
+        A[i] = rand() % 2 - 1;
+    }
+    for (int i = 0; i < n; i++) {
+        if (A[i] != -1) {
+            A[i] = rand() % 20;
+        }
+    }
+    if (n <= 256) {
+        cout << "A: ";
+        for (auto x: A)
+            cout << x << " ";
+        cout << endl;
+    }
+
+    // compact the array A
+    double start_time, end_time;
+    start_time = omp_get_wtime();
+    vector<int> B(n);
     #pragma omp parallel for
     for (int i = 0; i < n; i++) {
         B[i] = (A[i] != -1) ? 1 : 0;
     }
-    int* C = prefix_sum(B, n, [](int x) {return x;}, [](int x, int y) {return x + y;});
-    // vector<int> reduced_array(C[n - 1]);
-    int* reduced_array = new int[C[n - 1]];
+    vector<int> C = prefix_sum(B, n, [](int x) {return x;}, [](int x, int y) {return x + y;});
+    vector<int> compacted_array(C[n - 1]);
 
     #pragma omp parallel for
     for (int i = 0; i < n; i++) {
         if (B[i]) {
-            reduced_array[C[i] - 1] = A[i];
+            compacted_array[C[i] - 1] = A[i];
         }
     }
-    return make_pair(reduced_array, C[n-1]);
+    end_time = omp_get_wtime();
+
+    cout << "Size of compacted array: " << C[n - 1] << endl;
+    if (n <= 256) {
+        cout << "compacted array: ";
+        for (auto x: compacted_array)
+            cout << x << " ";
+        cout << endl;
+    }
+    cout << "Time taken: " << end_time - start_time << " s" << endl;
+    
+}
+
+void doprefixsum(int n, int option) {
+    vector<int> A(n);
+    if (option == 1) {
+        for (int i = 0; i < n; i++) {
+            A[i] = rand() % 20;
+        }
+    } else if (option == 2) {
+        for (int i = 0; i < n; i++) {
+            A[i] = 1;
+        }
+    } else {
+        for (int i = 0; i < n; i++) {
+            A[i] = i + 1;
+        }
+    }
+
+    if (n <= 256) {
+        cout << "A: ";
+        for (auto x: A)
+            cout << x << " ";
+        cout << endl;
+    }
+
+    vector<int> B;
+    double start_time, end_time;
+    start_time = omp_get_wtime();
+    B = prefix_sum(A, n, [](int x) {return x;}, [](int x, int y) {return x + y;});
+    // B = inplace_prefix_sum(A, n, [](int x, int y) {return x + y;});
+    end_time = omp_get_wtime();
+    if (n <= 256) {
+        cout << "B: ";
+        for (int i = 0; i < n; i++)
+            cout << B[i] << " ";
+        cout << endl;
+    }
+    cout << "Last element of B: " << B[n-1] << endl;
+    cout << "Time taken : " << end_time - start_time << " s" << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -185,45 +245,8 @@ int main(int argc, char* argv[]) {
     }
     omp_set_num_threads(n_t);
 
-    int* A = new int[n]();
-
-    // for (int i = 0; i < n; i++) {
-    //     A[i] = rand() % 20 - 10;
-    // }
-    for (int i = 0; i < n; i++) {
-        A[i] = 1;
-    }
-    if (n <= 256) {
-        cout << "A: ";
-        for (int i = 0; i < n; i++) {
-            cout << A[i] << " ";
-        }
-        cout << endl;
-    }
-
-    int* prefixsum;
-    double start_time = omp_get_wtime();
-    prefixsum = prefix_sum(A, n, [](int x) {return x;}, [](int x, int y) {return x + y;});
-    // pair<int*, size_t> result = reduce_array(A, n);
-    // prefixsum = result.first;
-    // size_t k = result.second;
-    double end_time = omp_get_wtime();
-
-    // cout << "k: " << k << endl;
-    // n = k < n ? k : n;
-
-    // print out the array
-    if (n <= 256) {
-        cout << "B: ";
-        for (int i = 0; i < n; i++) {
-            cout << prefixsum[i] << " ";
-        }
-        cout << endl;
-    }
-    printf("Last number : %d\n", prefixsum[n-1]);
-    printf("Time taken : %f s\n", end_time-start_time);
-    delete[] A;
-    delete[] prefixsum;
+    // compact_array(n);
+    doprefixsum(n, 2);
 
     return 0;
 }
